511d43fef6711c626cad5e495d8937bc408f511b
SHOTBOI123 <sujaykamalakkanna@gmail.com>
2026-03-29 00:09:35 -0400

    Added Timer

---
+++ Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class CollectibleItem : MonoBehaviour
+{
+    public string itemType;   // "Stone" or "Marble"
+    public string itemId;     // "Stone_1", "Marble_2", etc.
+}
